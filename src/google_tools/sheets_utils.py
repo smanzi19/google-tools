90bcba9f89489bf9858b83934935a642389cfacc
Sam Manzi <smanzi@Sams-MBP.lan>
2021-05-09 16:30:50 -0400

    Clean up sheet_utils

--- src/google_tools/sheets_utils.py
+++ src/google_tools/sheets_utils.py
@@ -1,14 +1,12 @@
-from __future__ import print_function
 import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-# The ID and range of a sample spreadsheet.
+# Sample sheet
 SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
 SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 BUILD_PARAMS = {'serviceName':'sheets',
@@ -22,13 +20,9 @@ def Create_Service(build_params):
     Prints values from a sample spreadsheet.
     """
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    # Search for token creds. If scope permissions change, token.json should be deleted so it will be reissued.
     if os.path.exists(f'{CREDENTIALS_PATH}/token.json'):
-        print(SCOPES)
         creds = Credentials.from_authorized_user_file(F'{CREDENTIALS_PATH}/token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -36,23 +30,8 @@ def Create_Service(build_params):
             flow = InstalledAppFlow.from_client_secrets_file(
                 f'{CREDENTIALS_PATH}/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open(f'{CREDENTIALS_PATH}/token.json', 'w') as token:
             token.write(creds.to_json())
     build_params['credentials'] = creds
-#     service = build('sheets', 'v4', credentials=creds)
     service = build(**build_params)
     return service
-#     # Call the Sheets API
-#     sheet = service.spreadsheets()
-#     result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-#                                 range=SAMPLE_RANGE_NAME).execute()
-#     values = result.get('values', [])
-
-#     if not values:
-#         print('No data found.')
-#     else:
-#         print('Name, Major:')
-#         for row in values:
-#             # Print columns A and E, which correspond to indices 0 and 4.
-#             print('%s, %s' % (row[0], row[4]))
